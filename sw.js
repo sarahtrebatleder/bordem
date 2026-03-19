50d96f3c9e443b4bb54ead4d8d0c2ea1bc3a8151
sarahtrebatleder <sarahtrebatleder@gmail.com>
2026-03-18 20:36:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'activity-picker-v13';
+const CACHE = 'activity-picker-v14';
 const ASSETS = [
   '/bordem/',
   '/bordem/index.html',
@@ -6,6 +6,21 @@ const ASSETS = [
   '/bordem/icons/icon-192.png',
   '/bordem/icons/icon-512.png',
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 
 self.addEventListener('install', e => {
   e.waitUntil(
